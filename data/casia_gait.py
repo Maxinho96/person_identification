133831cd7a3e8465798460565d63fe7eae5f56c0
Massimiliano Bruni <massimiliano.bruni@icloud.com>
2020-03-31 08:52:35 +0200

    Optimized disk usage when splitting casia_gait

--- data/casia_gait.py
+++ data/casia_gait.py
@@ -2,18 +2,27 @@ from absl import app, flags
 from absl.flags import FLAGS
 import os
 import cv2
+from zipfile import ZipFile
 import tarfile
 
 flags.DEFINE_string("dataset_dir", "casia_gait/DatasetB-1",
                     "Path to original CASIA Gait dataset")
 flags.DEFINE_string("output_dir", "casia_gait/DatasetB_split",
                     "Target directory to save the new dataset")
+flags.DEFINE_string("zip_pwd", None,
+                    "Dataset zip password, if necessary")
 
 
-# Extracts and deletes all .tar.gz files inside a path
+# Extracts and deletes all .zip and .tar.gz files inside a path
 def extract_and_delete(path):
     for file_name in os.listdir(path):
-        if file_name.endswith(".tar.gz"):
+        if file_name.endswith(".zip"):
+            archive_path = os.path.join(path, file_name)
+            pwd = bytes(FLAGS.zip_pwd, "utf-8")
+            with ZipFile(archive_path, "r") as archive:
+                archive.extractall(path=path, pwd=pwd)
+            os.remove(archive_path)
+        elif file_name.endswith(".tar.gz"):
             archive_path = os.path.join(path, file_name)
             archive = tarfile.open(archive_path)
             archive.extractall(path=path)
@@ -43,7 +52,8 @@ def get_file_dest(file_name):
     return dest
 
 
-def split_video(video_path, dest_dir):
+# Splits a video into frames and applies masks to get silhouettes
+def split_video(video_path, dest_dir, silhouettes_dir):
     # Take the video name and cut .avi
     video_name = os.path.basename(video_path)[:-4]
 
@@ -52,44 +62,62 @@ def split_video(video_path, dest_dir):
     success, image = vidcap.read()
     while success:
         # Image file name is video name followed by 3-digit frame number
-        img_name = "{}-{:03}.jpg".format(video_name, frame_num)
-        img_path = os.path.join(dest_dir, img_name)
-        cv2.imwrite(img_path, image)
+        image_name = "{}-{:03}".format(video_name, frame_num)
+
+        image_masked = apply_mask(image, image_name, silhouettes_dir)
+
+        image_path = os.path.join(dest_dir, image_name + ".jpg")
+        if image_masked is not None and not os.path.exists(image_path):
+            cv2.imwrite(image_path, image_masked)
+
         success, image = vidcap.read()
         frame_num += 1
 
 
-# Applies .png masks to full images, to obtain silhouettes
-def apply_masks(images_dir, silhouettes_dir):
-    for file_name in os.listdir(images_dir):
-        if file_name.endswith(".jpg"):
-            image_path = os.path.join(images_dir, file_name)
-            image_name, *_ = file_name.split(".")
+# Applies the .png mask to an image, to obtain the silhouette
+def apply_mask(image, image_name, silhouettes_dir):
+    subject_id, walk_status, seq_num, view_angle, *_ = \
+        image_name.split("-")
 
-            subject_id, walk_status, seq_num, view_angle, *_ = \
-                image_name.split("-")
+    mask_dir = os.path.join(
+        silhouettes_dir,
+        subject_id,
+        walk_status + "-" + seq_num,
+        view_angle)
+    mask_name = image_name + ".png"
+    mask_path = os.path.join(mask_dir, mask_name)
 
-            mask_dir = os.path.join(
-                silhouettes_dir,
-                subject_id,
-                walk_status + "-" + seq_num,
-                view_angle)
-            mask_name = image_name + ".png"
-            mask_path = os.path.join(mask_dir, mask_name)
+    masked_image = None
+    # Not all images have their mask
+    if os.path.exists(mask_path):
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
 
-            # Not all images have their mask
-            if os.path.exists(mask_path):
-                image = cv2.imread(image_path)
-                mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
-                masked_image = cv2.bitwise_and(image, image, mask=mask)
+        if image.shape[:2] != mask.shape[:2]:
+            print(image_name, "size mismatch", mask_name)
 
-                cv2.imwrite(image_path, masked_image)
-            # Delete images that don't have their mask
-            else:
-                os.remove(image_path)
+            ver_dif = image.shape[0] - mask.shape[0]
+            hor_dif = image.shape[1] - mask.shape[1]
+
+            mask = cv2.copyMakeBorder(
+                mask,
+                0,
+                ver_dif,
+                0,
+                hor_dif,
+                cv2.BORDER_CONSTANT,
+                value=[0, 0, 0])
+
+        masked_image = cv2.bitwise_and(image, image, mask=mask)
+    # Return None if image doesn't have its mask
+    return masked_image
 
 
 def main(_argv):
+    # Extract dataset if it is not already extracted
+    parent_dir = os.path.abspath(os.path.join(FLAGS.dataset_dir, os.pardir))
+    extract_and_delete(parent_dir)
+
+    # Extract siilhouettes if they are not already extracted
     silhouettes_dir = os.path.join(FLAGS.dataset_dir, "silhouettes")
     extract_and_delete(silhouettes_dir)
 
@@ -103,9 +131,10 @@ def main(_argv):
             if not os.path.exists(dest_dir):
                 os.makedirs(dest_dir)
 
-            split_video(video_path, dest_dir)
+            print("Splitting", file_name)
+            split_video(video_path, dest_dir, silhouettes_dir)
 
-            apply_masks(dest_dir, silhouettes_dir)
+            # apply_masks(dest_dir, silhouettes_dir)
 
 
 if __name__ == "__main__":
