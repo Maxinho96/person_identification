a84279894bb6acb8949a506a285fded87d210ccb
Massimiliano Bruni <massimiliano.bruni@icloud.com>
2020-04-21 21:42:37 +0200

    Switched to Xception standard model.

--- models.py
+++ models.py
@@ -1,11 +1,15 @@
 import tensorflow.keras as keras
 
 
-def get_standard_model(num_classes):
+# Get an Xception model. It can be of a fixed size or accept any size.
+# The model is the same, but fixed size is more efficient if you don't
+# plan to use the model on different sizes.
+def get_model(num_classes, size=None):
     base_model = keras.applications.xception.Xception(
         # keras.applications.nasnet.NASNetLarge(
         weights="imagenet",
         include_top=False,
+        input_shape=(size, size, 3),
         pooling="avg"
     )
 
@@ -20,22 +24,23 @@ def get_standard_model(num_classes):
     return model
 
 
-def get_fcn_model(num_classes):
-    base_model = keras.applications.xception.Xception(
-        weights="imagenet",
-        include_top=False,
-        input_shape=(None, None, 3)
-    )
+# def get_fcn_model(num_classes):
+#     base_model = keras.applications.xception.Xception(
+#         weights="imagenet",
+#         include_top=False,
+#         input_shape=(None, None, 3)
+#     )
 
-    inputs = base_model.input
-    base_outputs = base_model.output
-    conv = keras.layers.Conv2D(filters=num_classes,
-                               kernel_size=1,
-                               strides=1,
-                               padding="same",
-                               name="conv1x1")(base_outputs)
-    outputs = keras.layers.GlobalAveragePooling2D(name="avg")(conv)
+#     inputs = base_model.input
+#     base_outputs = base_model.output
+#     conv = keras.layers.Conv2D(filters=num_classes,
+#                                kernel_size=1,
+#                                strides=1,
+#                                padding="same",
+#                                name="conv1x1")(base_outputs)
+#     avg = keras.layers.GlobalAveragePooling2D(name="avg")(conv)
+#     outputs = keras.activations.softmax(avg)
 
-    model = keras.Model(inputs=inputs, outputs=outputs)
+#     model = keras.Model(inputs=inputs, outputs=outputs)
 
-    return model
+#     return model
